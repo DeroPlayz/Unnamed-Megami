a0451b345f55c0fab06d4eccbaca7752f41e9967
DeroPlayz <88916740+DeroPlayz@users.noreply.github.com>
2023-11-30 17:50:17 +0000

    modified:   src/Entity/Entity.java

--- src/Entity/Entity.java
+++ src/Entity/Entity.java
@@ -1,20 +1,6 @@
 package Entity;
 
 public abstract class Entity{
-    {
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        protected final String Fool = "Fool";
-        
     private String Name; public String getName(){return Name;} public void setName(String Name){this.Name = Name;}
     private String Arcana; public String getArcana(){return Arcana;} public void setArcana(String Arcana){this.Arcana = Arcana;}
     private String
