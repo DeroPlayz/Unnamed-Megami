99f5c53864253bc986bee293a89c59d28b6355c9
DeroPlayz <deroplayz@gmail.com>
2023-12-02 14:14:32 -0800

    Yeah

--- src/Entity/Actor/Actor.java
+++ src/Entity/Actor/Actor.java
@@ -1,17 +1,9 @@
 import Arcanum.Arcana;
+import Combat.Skill;
 import Entity.Entity;
+import lib.MafLib;
 
 public abstract class Actor extends Entity{
-    protected Arcana Arcana; public void setArcana(Arcana Arcana){this.Arcana = Arcana;} public Arcana getArcana(){return Arcana;}
-    protected int HP; public void setHP(int HP){this.HP = HP;} public int getHP(){return HP;}
-    protected int MaxHP; public void setMaxHP(int MaxHP){this.MaxHP = MaxHP;} public int getMaxHP(){return MaxHP;}
-    protected int SP; public void setSP(int SP){this.SP = SP;} public int getSP(){return SP;}
-    protected int MaxSP; public void setMaxSP(int MaxSP){this.MaxSP = MaxSP;} public int getMaxSP(){return MaxSP;}
-    protected int Strength; public void setStrength(int Strength){this.Strength = Strength;} public int getStrength(){return Strength;}
-    protected int Magic; public void setMagic(int Magic){this.Magic = Magic;} public int getMagic(){return Magic;}
-    protected int Endurance; public void setEndurance(int Endurance){this.Endurance = Endurance;} public int getEndurance(){return Endurance;}
-    protected int Agility; public void setAgility(int Agility){this.Agility = Agility;} public int getAgility(){return Agility;}
-    protected int Luck; public void setLuck(int Luck){this.Luck = Luck;} public int getLuck(){return Luck;}
     /**
      * 01st. Physical<p>
      * 02nd. Fire (Also dictates affinity to Burn.)<p>
@@ -24,12 +16,23 @@ public abstract class Actor extends Entity{
      * 09th. Sleep<p>
      * 10th. Dizzy<p>
      */
-    private double[] Affinities;
-    private static final double Weak = 1.5;
-    private static final double None = 1;
-    private static final double Resist = 0.5;
-    private static final double Block = 0;
-    private static final double Absorb = -1;
+    protected double[] Affinities;
+        protected static final double Weak = 1.5;
+        protected static final double None = 1;
+        protected static final double Resist = 0.5;
+        protected static final double Block = 0;
+        protected static final double Absorb = -1;
+    protected Skill[] Skills;
+    protected Arcana Arcana; public void setArcana(Arcana Arcana){this.Arcana = Arcana;} public Arcana getArcana(){return Arcana;}
+    protected int HP; public void setHP(int HP){this.HP = HP;} public int getHP(){return HP;}
+    protected int MaxHP; public void setMaxHP(int MaxHP){this.MaxHP = MaxHP;} public int getMaxHP(){return MaxHP;}
+    protected int SP; public void setSP(int SP){this.SP = SP;} public int getSP(){return SP;}
+    protected int MaxSP; public void setMaxSP(int MaxSP){this.MaxSP = MaxSP;} public int getMaxSP(){return MaxSP;}
+    protected int Strength; public void setStrength(int Strength){this.Strength = Strength;} public int getStrength(){return Strength;}
+    protected int Magic; public void setMagic(int Magic){this.Magic = Magic;} public int getMagic(){return Magic;}
+    protected int Endurance; public void setEndurance(int Endurance){this.Endurance = Endurance;} public int getEndurance(){return Endurance;}
+    protected int Agility; public void setAgility(int Agility){this.Agility = Agility;} public int getAgility(){return Agility;}
+    protected int Luck; public void setLuck(int Luck){this.Luck = Luck;} public int getLuck(){return Luck;}
 
     public Actor(String Name, Arcana Arcana){
         super(Name);
@@ -39,5 +42,21 @@ public abstract class Actor extends Entity{
     @Override
     public String toString(){
         return Name + "(" + Arcana + ")";
+    }
+
+    public void attack(){
+        String response = MafLib.askString("1.Melee\n2.Spells\n3.Wait", true);
+        if(response.equals("1")){
+            
+        }
+        else if(response.equals("2")){
+
+        }
+        else if(response.equals("3")){
+            
+        }
+        else{
+            attack();
+        }
     }
 }
