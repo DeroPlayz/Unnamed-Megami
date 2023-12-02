bab31836777b711da446b19b1b0d1df516c11b27
DeroPlayz <deroplayz@gmail.com>
2023-12-02 10:54:07 -0800

    More testing.

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ import static lib.MafLib.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(UNDERLINE + Agi);
-        System.out.println(BLINK + Bufu);
-        System.out.println(STRIKE + Zio);
-        System.out.println(REVIDEO + Garu);
+        System.out.println(BLUE + Agi);
+        System.out.println(BOLD + Bufu);
+        System.out.println(CLEARF + Zio);
+        System.out.println(Garu);
         // System.out.println(RED + Agi);
         // System.out.println(RED + Agi);
     }
