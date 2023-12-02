68d43bb2536fd4adb16a37ebdc551505afae148e
DeroPlayz <deroplayz@gmail.com>
2023-12-02 12:28:17 -0800

    Slightly reworked constructor.

--- src/Entity/Actor/Player.java
+++ src/Entity/Actor/Player.java
@@ -1,12 +1,13 @@
 import Item.Item;
+import static Arcanum.Arcana.Fool;
 
 class Player extends Actor{
     private String First;
     private String Last;
     private Item[] Inventory = new Item[15];
     private double Cash;
-    
+
     public Player(String First, String Last){
-        super(First + Last, Arcana.Fool);
+        super((First + " " + Last), Fool);
     }
 }
