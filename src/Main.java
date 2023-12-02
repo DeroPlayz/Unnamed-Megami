9d56c6e94ee7c0b86dba37b0c67da867b1656b26
DeroPlayz <deroplayz@gmail.com>
2023-12-02 10:16:51 -0800

    Testing colors.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+
+import static Combat.Skill.*;
+import static lib.MafLib.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println(UNDERLINE + Agi);
+        System.out.println(BLINK + Bufu);
+        System.out.println(STRIKE + Zio);
+        System.out.println(REVIDEO + Garu);
+        // System.out.println(RED + Agi);
+        // System.out.println(RED + Agi);
     }
 }
