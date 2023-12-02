3951836a31d6b53e42c4b55d1a7e27817751ad7c
DeroPlayz <deroplayz@gmail.com>
2023-12-02 11:28:42 -0800

    Reworking elements.

--- src/Combat/Element.java
+++ src/Combat/Element.java
@@ -1,30 +1,33 @@
 package Combat;
+import static Combat.Status.*;
 
 public class Element{
     private String Name; public void setName(String Name){this.Name = Name;} public String getName(){return Name;}
     private static int IDc;
     private int ID;
-    private boolean isOffense;
     private Status Ailment;
 
-    public Element(String Name, boolean isOffense){
+    public Element(String Name){
         this.Name = Name;
-        this.isOffense = isOffense;
+        this.Ailment = Alive;
+        ID = IDc;
+        IDc++;
+    }
+
+    public Element(String Name, Status Ailment){
+        this.Name = Name;
+        this.Ailment = Ailment;
         ID = IDc;
         IDc++;
     }
     
-    //Offense Magic.
-        public static Element Physical = new Element("Physical", true);
-        public static Element Fire = new Element("Fire", true);
-        public static Element Ice = new Element("Ice", true);
-        public static Element Electric = new Element("Electric", true);
-        public static Element Wind = new Element("Wind", true);
-        public static Element Bless = new Element("Bless", true);
-        public static Element Curse = new Element("Curse", true);
-
-    //Not Offense Magic.
-        public static Element Support = new Element("Support", false);
-        public static Element Recovery = new Element("Recovery", true);
+    public static Element Physical = new Element("Physical");
+    public static Element Fire = new Element("Fire");
+    public static Element Ice = new Element("Ice");
+    public static Element Electric = new Element("Electric");
+    public static Element Wind = new Element("Wind");
+    public static Element Bless = new Element("Bless");
+    public static Element Curse = new Element("Curse");
+    public static Element Support = new Element("Support");
 
 }
