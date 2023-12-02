32f7791d5d4017ea82372d106f9ec6b051aaf101
DeroPlayz <deroplayz@gmail.com>
2023-12-02 12:32:39 -0800

    Added some getters and setters.

--- src/Combat/Element.java
+++ src/Combat/Element.java
@@ -4,8 +4,8 @@ import static Combat.Status.*;
 public class Element{
     private String Name; public void setName(String Name){this.Name = Name;} public String getName(){return Name;}
     private static int IDc;
-    private int ID;
-    private Status Ailment;
+    private int ID; public int getID(){return ID;}
+    private Status Ailment; public void setAilment(Status Ailment){this.Ailment = Ailment;} public Status getAilment(){return Ailment;}
 
     public Element(String Name){
         this.Name = Name;
@@ -22,9 +22,9 @@ public class Element{
     }
     
     public static Element Physical = new Element("Physical");
-    public static Element Fire = new Element("Fire");
-    public static Element Ice = new Element("Ice");
-    public static Element Electric = new Element("Electric");
+    public static Element Fire = new Element("Fire", Burn);
+    public static Element Ice = new Element("Ice", Freeze);
+    public static Element Electric = new Element("Electric", Stun);
     public static Element Wind = new Element("Wind");
     public static Element Bless = new Element("Bless");
     public static Element Curse = new Element("Curse");
