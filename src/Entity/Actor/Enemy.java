d610f5f9f8e4f3ff0b7fc8a50eb0fe35a433c1ea
DeroPlayz <deroplayz@gmail.com>
2023-12-02 14:14:26 -0800

    Created Enemy Party.

--- src/Entity/Actor/Enemy.java
+++ src/Entity/Actor/Enemy.java
@@ -1,15 +1,15 @@
 import static Combat.Skill.*;
 
+import java.util.ArrayList;
+
 import Arcanum.Arcana;
 
 import Combat.Skill;
 
 public class Enemy extends Actor {
-    private Skill[] Skills;
-
     public Enemy(String Name, Arcana Arcana, int MaxHP, int MaxSP, int[] SMEAL, Skill[] Skills) {
         super(Name, Arcana);
-        this.Skills = Skills;
+        super.Skills = Skills;
         this.MaxHP = HP = MaxHP;
         this.MaxSP = SP = MaxSP;
         Strength = SMEAL[0];
@@ -19,6 +19,21 @@ public class Enemy extends Actor {
         Luck = SMEAL[4];
     }
 
-    Enemy Pixie = new Enemy("Pixie", Arcanum.Arcana.Lovers, 15, 15, new int[]{0, 0, 0, 0, 0}, new Skill[]{Dia});
-   
+    final Enemy Pixie = new Enemy("Pixie", Arcanum.Arcana.Lovers, 15, 15, new int[]{0, 0, 0, 0, 0}, new Skill[]{Dia});
+    public static Enemy eOne;
+    public static Enemy eTwo;
+    public static Enemy eThree;
+    public static Enemy eFour;
+    public static Enemy eFive;
+    public static Enemy eSix;
+
+    public static ArrayList<Enemy> EnemyParty = new ArrayList<>(){{
+        add(eOne);
+        add(eTwo);
+        add(eThree);
+        add(eFour);
+        add(eFive);
+        add(eSix);
+    }};
+
 }
