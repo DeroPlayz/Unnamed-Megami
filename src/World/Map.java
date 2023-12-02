24844c7585e5d3a65bb05ce9b54ec665e878d0fe
DeroPlayz <deroplayz@gmail.com>
2023-12-01 21:00:58 -0800

    Fiddled with the map system.

--- src/World/Map.java
+++ src/World/Map.java
@@ -1,11 +1,14 @@
 package World;
 
+import java.util.HashMap;
+
 public class Map {
     private String Name; public void setName(String s){Name = s;} public String getName(){return Name;}
     private int minX; public void setMinX(int minX){this.minX = minX;} public int getMinX(){return minX;}
     private int maxX; public void setMaxX(int maxX){this.maxX = maxX;} public int getMaxX(){return maxX;}
     private int minZ; public void setMinZ(int minZ){this.minZ = minZ;} public int getMinZ(){return minZ;}
     private int maxZ; public void setMaxZ(int maxZ) {this.maxZ = maxZ;} public int getMaxZ() {return maxZ;}
+    private HashMap<Location, int[]> Locations = new HashMap<>();
 
     public Map(String Name, int minX, int maxX, int minZ, int maxZ){
         this.Name = Name;
@@ -22,5 +25,5 @@ public class Map {
     }
 
     Map World = new Map("World Map", 0, 100, 0, 100);
-    Map Home = new Map("Home");
+    Map Home = new Map("Home", 0, 10, 0, 10);
 }
