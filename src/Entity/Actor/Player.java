c2e409ff164be6fdb0857728b416da08b4051371
DeroPlayz <deroplayz@gmail.com>
2023-12-01 18:51:12 -0800

    Added inventory and cash stats.

--- src/Entity/Actor/Player.java
+++ src/Entity/Actor/Player.java
@@ -1,6 +1,11 @@
+import Item.Item;
+
 class Player extends Actor{
     private String First;
     private String Last;
+    private Item[] Inventory = new Item[15];
+    private double Cash;
+    
     public Player(String First, String Last){
         super(First + Last, Arcana.Fool);
     }
