406d1a9ee1a451a3389f4dc651bb27b431bf4cdb
DeroPlayz <deroplayz@gmail.com>
2023-12-03 11:22:36 -0800

    Oh boy. Started work on the actual world map.

--- src/World/Map.java
+++ src/World/Map.java
@@ -1,22 +1,68 @@
 package World;
-import static World.Location.*;
+import static World.Tile.*;
 
 public class Map {
     private String Name; public void setName(String s){Name = s;} public String getName(){return Name;}
-    private Location[][] Places;
+    private Tile[][] Tiles;
 
-    public Map(String Name, Location[][] Places){
-        this.Name = Name;
-        this.Places = Places;
+    public Map(String Name, Tile[][] Tiles){
+    this.Name = Name;
+    this.Tiles = Tiles;
     }
 
-    public static Map World = new Map("World Map", new Location[][]{{Wall, Wall, Wall, Wall, Wall, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path}});
+    public static Map World = new Map("World Map", new Tile[][]{
+    /*Y=0*/     {SCWall, SCWall, SCWall, SCWall, SCWall, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, PLWall, PLWall, PLWall, PLWall, PLWall, PLWall, PLWall, PLWall},
+    /*Y=1*/     {SCWall, Coliseum, Coliseum, Coliseum, SCWall, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=2*/     {SCWall, Coliseum, Coliseum, Coliseum, SCWall, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=3*/     {SCWall, Coliseum, Coliseum, Coliseum, SCWall, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=4*/     {SCWall, SCWall, SCWall, SCWall, SCWall, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=5*/     {Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=6*/     {Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=7*/     {Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=8*/     {Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=9*/     {Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=10*/    {Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=11*/    {Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, Prison, Prison, Prison, Prison, Prison, Prison, Prison, PLWall},
+    /*Y=12*/    {Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, PLWall, PLWall, PLWall, PLWall, PLWall, PLWall, PLWall, PLWall, PLWall},
+    /*Y=13*/    {Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Mountain, Mountain, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Mountain, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path},
+    /*Y=14*/    {Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Mountain, Mountain, Path, Path, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path},
+    /*Y=15*/    {Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Path, Path, Mountain, Mountain, Mountain, Mountain, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path},
+    /*Y=16*/    {Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path},
+    /*Y=17*/    {Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path},
+    /*Y=18*/    {Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path},
+    /*Y=19*/    {Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Path, Path, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path},
+    /*Y=20*/    {Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Mountain, Home, Path, Path, Path, Path, Path, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path},
+    /*Y=21*/    {Path, Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path},
+    /*Y=22*/    {Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Mountain, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path, Path},
+    /*Y=23*/
+    /*Y=24*/
+    /*Y=25*/
+    /*Y=26*/
+    /*Y=27*/
+    /*Y=28*/
+    /*Y=29*/
+    /*Y=30*/
+    /*Y=31*/
+    /*Y=32*/
+    /*Y=33*/
+    /*Y=34*/
+    /*Y=35*/
+    /*Y=36*/
+    /*Y=37*/
+    /*Y=38*/
+    /*Y=39*/
+    /*Y=40*/
+    });
     
     public String toString(){
         String s = "";
-        for(int i = 0; i < Places.length; i++){
-            for(int j = 0; j < Places[i].length; j++){
-                s += Places[i][j];
+        for(int i = 0; i < Tiles.length; i++){
+            System.out.println("Y=" + i + " Length: " + Tiles[i].length);
+            for(int j = 0; j < Tiles[i].length; j++){
+                s += Tiles[i][j].toString() + " ";
+                if(Tiles[i][j].getLetter().length() == 1){
+                    s += " ";
+                }
             }
             s += "\n";
         }
