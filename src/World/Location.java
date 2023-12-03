98a6b0d901dbbc4a742b87b18f17f4ff62a44abb
DeroPlayz <deroplayz@gmail.com>
2023-12-02 19:08:55 -0800

    Maybe figured out how the concept of space will work

--- src/World/Location.java
+++ src/World/Location.java
@@ -1,5 +1,15 @@
 package World;
 
 public class Location{
-    
+    private String Name;
+    private boolean canSave;
+
+    public Location(String Name, boolean canSave){
+        this.Name = Name;
+        this.canSave = canSave;
+    }
+
+    public static final Location Home = new Location("Home", true);
+    public static final Location Path = new Location("Path", false);
+    public static final Location Mountain = new Location("Path", false);
 }
