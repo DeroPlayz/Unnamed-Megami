94227179e3dae5320861ad36208f32e491124faa
DeroPlayz <deroplayz@gmail.com>
2023-12-02 21:08:08 -0800

    reworked locations again

--- src/World/Location.java
+++ src/World/Location.java
@@ -3,13 +3,19 @@ package World;
 public class Location{
     private String Name;
     private boolean canSave;
+    private String letter;
 
-    public Location(String Name, boolean canSave){
+    public Location(String Name, boolean canSave, String Letter){
         this.Name = Name;
         this.canSave = canSave;
     }
 
-    public static final Location Home = new Location("Home", true);
-    public static final Location Path = new Location("Path", false);
-    public static final Location Mountain = new Location("Path", false);
+    public String toString(){
+        return letter;
+    }
+
+    public static final Location Home = new Location("Home", true, "H");
+    public static final Location Path = new Location("Path", false, "P");
+    public static final Location Mountain = new Location("Path", false, "M");
+    public static final Location Wall = new Location("Wall", false, "W");
 }
